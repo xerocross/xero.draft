f326f41e062951c21de7bcac9c584843465f63ae
xerocross <nonya@business.com>
2019-04-26 01:58:46 -0400

    Form textarea now logs input changes to console

--- src/components/MyStatefulComponent.jsx
+++ src/components/MyStatefulComponent.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from "react";
+import "./MyStatefulComponent.css";
 
 class MyStatefulComponent extends Component {
     constructor() {
         super();
+        this.state = {
+            text : ""
+        }
+        this.handleTextChange = this.handleTextChange.bind(this);
     }
+    history = [];
+    stateBuilder () {
+
+    }
+
+    handleTextChange (event) {
+        console.log(event.target.value);
+        this.setState({text: event.target.value});
+    }
+
     render() {
-        return ("I'm here!");
+        return (
+            <div>
+                <p>
+                    <textarea 
+                        className = "myTextarea" 
+                        onChange = {this.handleTextChange}
+                        value = {this.state.text}
+                    >
+                    </textarea>
+                </p>
+                <p>
+                    <input type="button" value="commit"/>
+                </p>
+            </div>
+        );
     }
 }
 export default MyStatefulComponent;
