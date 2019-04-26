439195871f0c4bae96b86640bdb39b27d80611b5
xerocross <nonya@business.com>
2019-04-26 00:33:16 -0400

    compose components and add 'I'm here'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import MyStatefulComponent from "./components/MyStatefulComponent";
 
 function App() {
   return (
     <div className="App">
-      
+      <MyStatefulComponent />
     </div>
   );
 }
--- src/components/MyStatefulComponent.jsx
+++ src/components/MyStatefulComponent.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 class MyStatefulComponent extends Component {
-
-    
+    constructor() {
+        super();
+    }
+    render() {
+        return ("I'm here!");
+    }
 }
+export default MyStatefulComponent;
