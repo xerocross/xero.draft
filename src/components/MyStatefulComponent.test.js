3be5b1b19366cfc3b2286dab904c1e9a0129034c
xerocross <nonya@business.com>
2019-04-27 01:57:41 -0400

    one test added

--- src/components/MyStatefulComponent.test.js
+++ src/components/MyStatefulComponent.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MyStatefulComponent from './MyStatefulComponent.jsx';
+import { render, fireEvent } from 'react-testing-library';
 
-it('renders without crashing', () => {
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MyStatefulComponent />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("commit text and reset to last commmit function as expected", ()=> {
+    const {  getByText, container } = render(<MyStatefulComponent />)
+    let textarea = container.querySelector("textarea");
+    fireEvent.change(textarea, { target: { value: 'adam' } });
+    let commitButton = getByText("commit");
+    fireEvent.click(commitButton);
+    fireEvent.change(textarea, { target: { value: 'adam cross' } });
+    let resetButton = getByText("reset to last commit");
+    fireEvent.click(resetButton);
+    expect(textarea.value).toBe("adam");
 });
